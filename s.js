91f7aef18095bad4f3b4b282fd2d47988d0811c1
leonardoigor <igor_mendonca@outlook.com.br>
2019-01-27 23:04:41 -0200

    tt

--- s.js
+++ s.js
@@ -12,14 +12,14 @@ $(document).ready(function()
 
     $('.colum').css({margin:"5px"});
 
-    $('.window').css({background:"#fff",width:"172px",display:"flex",
+    $('.window').css({background:"#fff",width:"310px",display:"flex",
 "justify-content":"center","align-items":"center","flex-direction":"column"
 ,border:"1px solid darkgrey",
-"box-shadow":"0px 0px 5px"});
+"box-shadow":"0px 0px 5px","border-radius": "20px"});
 
     $('input').hide()
     .delay(1000)
-    .fadeIn('1000');
+    .fadeIn('1000').css({});
 });
 
 document.getElementById('');
