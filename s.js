e306d115c920298421b04cb340e2da30a085bf79
leonardoigor <igor_mendonca@outlook.com.br>
2019-01-27 22:25:57 -0200

    .

---
+++ s.js
@@ -0,0 +1,26 @@
+// document.getElementById("text").addEventListener("keyup", a);
+
+// stylle
+$(document).ready(function()
+{
+    $('.contain').css({background:"#eee"
+    ,margin:"0 center"
+    ,display:"flex"
+    ,"justify-content":"center"
+    ,"align-items":"center"
+    ,"flex-direction":"column",height: "100vh"});
+
+    $('.colum').css({margin:"5px"});
+
+    $('.window').css({background:"#fff",width:"172px",display:"flex",
+"justify-content":"center","align-items":"center","flex-direction":"column"
+,border:"1px solid darkgrey",
+"box-shadow":"0px 0px 5px"});
+
+    $('input').hide()
+    .delay(1000)
+    .fadeIn('1000');
+});
+
+document.getElementById('');
+document.getElementById('');
